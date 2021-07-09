901436532b5b15eaaf52403abf2a5e6d71f8e844
heidichen90 <toddypet@gmail.com>
2021-07-09 22:15:39 +0100

    configure login page

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const exhbs = require("express-handlebars");
 const PORT = 3000;
 
 //set up  handlebars as a view engine
-app.engine("hbs", exhbs({ defaultLayout: "main" }));
+app.engine("hbs", exhbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
 
 //set up body parser
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Landing Page");
+  res.render("index");
 });
 
 app.listen(PORT, () => {
