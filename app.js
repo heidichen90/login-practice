0a9f60fd21d92c50344db2f785447077afe830c9
heidichen90 <toddypet@gmail.com>
2021-07-18 00:14:20 +0100

    add cookie session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const exhbs = require("express-handlebars");
 const Login = require("./models/login");
+const cookieParser = require("cookie-parser");
 require("./config/mongoose");
 
 const PORT = 3000;
@@ -9,12 +10,22 @@ const PORT = 3000;
 //set up  handlebars as a view engine
 app.engine("hbs", exhbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
+app.use(cookieParser());
 
 //set up body parser
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+  if (req.cookies.userName && req.cookies.userEmail) {
+    return res.render("welcome", { userName: req.cookies.userName });
+  }
   res.render("index");
+});
+
+app.get("/logout", (req, res) => {
+  res.clearCookie("userName");
+  res.clearCookie("userEmail");
+  res.redirect("/");
 });
 
 app.post("/login", (req, res) => {
@@ -29,6 +40,12 @@ app.post("/login", (req, res) => {
         res.render("index", { error });
       }
       if (loginDetail.password === user.password) {
+        //send cookie
+        res.cookie("userName", user.firstName, { maxAge: 1000 * 60 * 60 * 24 });
+        res.cookie("userEmail", user.email.toString(), {
+          maxAge: 1000 * 60 * 60 * 24,
+        });
+
         res.render("welcome", { userName: user.firstName });
       } else {
         error = "wrong password, please try it again";
